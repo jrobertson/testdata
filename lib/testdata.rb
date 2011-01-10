7b6b822b6d00de4c6b16109ae8977edd17137a27
James Robertson <james@r0bertson.co.uk>
2011-01-10 15:46:49 +0000

    generation of a log file is now optional

--- lib/testdata.rb
+++ lib/testdata.rb
@@ -2,23 +2,28 @@
 
 # file: testdata.rb
 
+require 'builder'
 require 'rexml/document'
 
 class Path
   include REXML
 
-  def initialize(doc, success) @doc, @success = doc, success end
+  def initialize(doc, log, success) 
+    @doc, @log, @success = doc, log, success 
+  end
 
   def tested? description
 
     stringify = Proc.new {|x| x.text.to_s.gsub(/[\n\s]/,'').length > 0 ? x.text : x.cdatas.join.strip}
 
     node = XPath.first(@doc.root, "records/test[summary/description='#{description}']")
+    raise "Path error: node title not found" unless node
     input_values = XPath.match(node, "records/io/summary[type='input']/*").map(&stringify)
     output_values = XPath.match(node, "records/io/summary[type='output']/*").map(&stringify)
 
     path_no = node.text('summary/path')
-    raise "Path error: node not found" unless node
+    actual = XPath.first(@log.root, "records/result[path_no='#{path_no}']/actual") if @log
+
     
     result = nil
     @success << [nil, path_no.to_i]      
@@ -33,12 +38,19 @@ class Path
       raw_result =  test(*values)
 
       if raw_result then
-        result = [raw_result].flatten.map(&:strip) == expected.map(&:strip)
+        a = [raw_result].flatten.map(&:strip)
+        b = expected.map(&:strip)
+        actual.text = a.join("\n")[/</] ? CData.new("\n%s\n" % a.join("\n")) : a.join("\n") if @log
+
+        # puts 'a : ' + a.inspect + ' a :' + a[0].length.to_s
+        # puts 'b : ' + b.inspect + ' b :' + b[0].length.to_s
+        result = a == b
       else
         result = [raw_result].compact == expected
       end
 
     rescue
+      puts 'error: ' + ($!).to_s
       result = false
     ensure
       @success[-1][0] = result
@@ -54,16 +66,28 @@ class Testdata
 
   attr_reader :success  
  
-  def initialize(s)
+  def initialize(s, options={})
     #puts 'filex : ' + $0
+    #puts 'filez : ' + __FILE__
+    #puts 'pwd : ' + Dir.pwd
+    #exit
+
+    o = {log: false}.merge(options)
+
     @filepath = File.dirname s
     buffer = self.send('read_' + (s[/https?:\/\//] ? 'file' : 'url'), s)   
     @doc = Document.new(buffer)
     raise "Testdata error: doc %s not found" % s unless @doc
     @success = []
+
+    @log =  o[:log] == true ? Document.new(tests) : nil
   end
 
-  def paths() yield(path = Path.new(@doc, @success)) end
+  def paths() 
+    yield(path = Path.new(@doc, @log, @success)) 
+    File.open(Dir.pwd + '/.test_log.xml','w'){|f| f.write @log } if @log
+  end
+
   def read_file(s) File.open(s, 'r').read  end
   def read_url(xml_url)  open(xml_url, 'UserAgent' => 'S-Rscript').read  end
 
@@ -74,6 +98,12 @@ class Testdata
     {false: @success.select{|x| x[0] == false}.map(&:last).sort,
      nil: ((a[0]..a[-1]).to_a - a)}
   end
+  
+  def find_by(s)
+    XPath.match(@doc.root, "records/test/summary[type='#{s}']/description/text()").map(&:to_s)
+  end
+
+  private
 
   def tests()
     script = XPath.first(@doc.root, "summary/script/text()").to_s
@@ -99,22 +129,54 @@ class Testdata
       path_no = node.text('summary/path').to_s
       output_values = XPath.match(node, "records/io/summary[type='output']/*").map(&stringify)
       raw_output = (output_values - ['output'])
-      output = "#=> " + raw_output.join("\n")
+      output = raw_output.join("\n")
 
       values = input_values - ['input']
       body = content[i][2].strip.gsub("\n      ","\n")
       if content[i][1] then
-        content[i][1].zip(values).each do |keyword, value|
-          body.gsub!(/#{keyword}(?!:)/, value)
+        content[i][1].zip(values).reverse.each do |keyword, value|
+          body.gsub!(/#{keyword}(?!:)/, "'%s'" % value )
         end
       end
-      ["# #{path_no}) #{content[i][0]}\n", body + "\n", output + "\n\n"]  
+      [path_no, content[i][0], body, output]  
     end
 
-    r.each {|x| x.join("\n")}.join("\n")
+    #r.each {|x| x.join("\n")}.join("\n")
+    tests_to_dynarex(r)
   end
-  
-  def find_by(s)
-    XPath.match(@doc.root, "records/test/summary[type='#{s}']/description/text()").map(&:to_s)
+
+  def tests_to_dynarex(tests)
+
+    xml = Builder::XmlMarkup.new( target: buffer='', indent: 2 )
+    xml.instruct! :xml, version: "1.0", encoding: "UTF-8"
+
+    xml.results do
+      xml.summary do
+        xml.recordx_type 'dynarex'
+        xml.format_mask '[!path_no] [!title] [!test] [!expected] [!actual]'
+        xml.schema 'results/result(path_no, title, test, expected, actual)'
+      end
+      xml.records do
+        tests.each do |path_no, title, test, expected|
+          xml.result do
+            xml.path_no path_no
+            xml.title title
+
+            xml.test do
+              test[/[<>]/] ? xml.cdata!("\n%s\n" % test) : test
+            end
+
+            xml.expected do
+              expected[/</] ? xml.cdata!("\n%s\n" % expected) : expected
+            end
+
+            xml.actual
+          end
+        end
+      end
+    end    
+
+    buffer
   end
+
 end
--- testdata.gemspec
+++ testdata.gemspec
@@ -1,6 +1,7 @@
 Gem::Specification.new do |s|
   s.name = 'testdata'
-  s.version = '0.2.9'
+  s.version = '0.3.1'
   s.summary = 'testdata'
   s.files = Dir['lib/**/*.rb']
+  s.add_dependency('builder')
 end
