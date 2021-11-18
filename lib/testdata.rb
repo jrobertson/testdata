44e501be91e44dcc3745723ee1e05626da3719bc
James Robertson <james@r0bertson.co.uk>
2021-11-18 11:48:05 +0000

    bug fix: URI.open now explicitly stated, as required in Ruby 3.0.2.

--- lib/testdata.rb
+++ lib/testdata.rb
@@ -13,7 +13,7 @@ class TestdataException < Exception
 end
 
 module Testdata
-  
+
   class Base
     using ColouredText
 
@@ -22,7 +22,7 @@ module Testdata
     attr_accessor :debug
 
     def initialize(s, options={})
-      
+
       super()
 
       @params = {}
@@ -33,14 +33,14 @@ module Testdata
       # open the testdata document
       procs = {
         String: proc {|x|
-                      
+
           if x.strip[/^</] then
             x
           elsif x[/https?:\/\//] then
             read_url x
           else
             read_file x
-          end                   
+          end
         },
         Polyrex: proc {|x| x.to_xml}
       }
@@ -48,26 +48,26 @@ module Testdata
       buffer =  procs[s.class.to_s.to_sym].call(s)
 
       @doc = Rexle.new(buffer)
-      
+
       o = {log: false}.merge(options)
 
       @log =  o[:log] == true ? Rexle.new(tests) : nil
     end
 
     def run(raw_x=nil, debug2=nil)
-      
+
       # verify the document  has unique path numbers
-      
+
       a = @doc.root.xpath('records/test/summary/path/text()')
       duplicates = a.select{ |e| a.count(e) > 1 }.uniq
-      
+
       if duplicates.any? then
-        raise 'Duplicate path found. Path: ' + duplicates.inspect 
+        raise 'Duplicate path found. Path: ' + duplicates.inspect
       end
 
       @debug2 = debug2 ? true : false
       @success = []
-      
+
       x = if raw_x and raw_x[/\d+\.\.\d+/] then
         x, y = raw_x.split('..').map(&:to_i)
         id = Range.new(x,y)
@@ -75,15 +75,15 @@ module Testdata
         raw_x
       end
 
-      procs = {NilClass: :test_all, Range: :test_range, String: :test_id, 
+      procs = {NilClass: :test_all, Range: :test_range, String: :test_id,
                Integer: :test_id, Fixnum: :test_id}
 
       method(procs[x.class.to_s.to_sym]).call(x)
       summary()
     end
-    
+
     private
-    
+
     def testdata_values(id)
 
       node = @doc.root.element "records/test[summary/path='#{id}']"
@@ -91,7 +91,7 @@ module Testdata
 
       path_no = node.text('summary/path')
 
-      input_summary = node.element 'records/input/summary'      
+      input_summary = node.element 'records/input/summary'
       input_summary.delete 'schema | format_mask | recordx_type'
 
       input_nodes = input_summary.xpath('*') #[1..-1]
@@ -105,8 +105,8 @@ module Testdata
 
       type, desc = summary.text('type'), summary.text('description')
 
-      output_summary = node.element 'records/output/summary'      
-      output_summary.delete 'schema | format_mask | recordx_type'      
+      output_summary = node.element 'records/output/summary'
+      output_summary.delete 'schema | format_mask | recordx_type'
 
       output_nodes = output_summary.xpath('*') #[1..-1]
       output_values = output_nodes.map{|x| x.texts.map(&:unescape).join.strip}
@@ -119,32 +119,32 @@ module Testdata
       x ||=(0..-1)
 
       break_on_fail = @doc.root.element('summary/break_on_fail/text()') == 'true'
-      
+
       test_id = nil
 
       begin
-        
+
         @doc.root.xpath("records/test/summary/path/text()")[x].each do |id|
-          
+
           test_id = id
           puts 'testing id: ' + id.inspect
           result = test_id(id)
-          break if result == false and break_on_fail 
-          
+          break if result == false and break_on_fail
+
         end
-        
+
       rescue
 
         @success << [false, test_id.to_i]
       end
-      
+
 
     end
 
     def test_id(id='')
 
 
-      path_no, inputs, input_names, type, expected, @desc = 
+      path_no, inputs, input_names, type, expected, @desc =
                                             testdata_values(id.to_s)
       @inputs = inputs
 
@@ -154,7 +154,7 @@ module Testdata
       puts  "warning: no test route found for " + type unless raw_actual
 
       result = nil
-      @success << [nil, path_no.to_i]      
+      @success << [nil, path_no.to_i]
 
       begin
 
@@ -164,7 +164,7 @@ module Testdata
           b = expected.map(&:strip)
 
           if @debug == true or @debug2 == true then
-            
+
             inputs = input_names.zip(inputs).map{|x| '  ' + x.join(": ")}\
               .join("\n")
 
@@ -176,18 +176,18 @@ module Testdata
 
           result = a.join.force_encoding("UTF-8") == \
               b.join.force_encoding("UTF-8")
-          
+
           if (@debug == true or @debug2 == true) and result == false then
 
             # diff the expected and actual valuess
             puts DiffyC32.new(b.first, a.first).to_s
-  
+
           end
         else
           result = [raw_actual].compact == expected
         end
 
-      rescue Exception => e  
+      rescue Exception => e
         err_label = e.message + " :: \n" + e.backtrace.join("\n")
         raise TestdataException,  err_label
         result = false
@@ -198,15 +198,15 @@ module Testdata
     end
 
     def test_range(range)
-      
+
       a = @doc.root.xpath("records/test/summary/path/text()")
       a[range].each {|x| test_id x}
     end
-    
+
     def tests(*args)
       # override this method in the child class
     end
-    
+
     def read_rdx(buffer)
       puts 'inside read_rdf: buffer: ' + buffer.inspect
       dx = Dynarex.new
@@ -232,7 +232,7 @@ module Testdata
       dx.all.each.with_index do |x, i|
 
         px.create.test(path: (i+1).to_s, type: dx.test_type) do |create|
-          
+
           raw_inputs.each do |fld_in|
 
             create.input(fld_in[0..-4].to_sym => \
@@ -248,17 +248,17 @@ module Testdata
       end
 
       px.to_xml
-        
+
     end
 
-    def read_file(s) 
+    def read_file(s)
       buffer = File.open(s, 'r').read
       ext = s[/\.(\w+)$/,1]
-      method(('read_' + ext).to_sym).call(buffer)    
+      method(('read_' + ext).to_sym).call(buffer)
     end
-      
+
     def read_url(url)
-      buffer = open(url, 'UserAgent' => 'Testdata').read  
+      buffer = URI.open(url, 'UserAgent' => 'Testdata').read
       ext = url[/.*\/[^\.]+\.(\w+)/,1]
       method(('read_' + ext).to_sym).call(buffer)
     end
@@ -266,7 +266,7 @@ module Testdata
     def read_xml(buffer)
       buffer
     end
-    
+
     def read_td(buffer)
       puts 'buffer: ' + buffer
       TestdataText.parse buffer
@@ -285,73 +285,73 @@ module Testdata
         score:  [success.grep(true), success].map(&:length).join('/'),
         failed:  @success.select{|x| x[0] == false}.map(&:last).sort
       }
-      
+
       def h.to_s()
         passed = self[:passed] ? self[:passed].to_s.light_green : self[:passed].to_s.light_red
-        "{passed: #{passed}, score: #{self[:score]}, failed: #{self[:failed]}}"        
-        
+        "{passed: #{passed}, score: #{self[:score]}, failed: #{self[:failed]}}"
+
       end
-      
+
       h
     end
   end
 
   class Unit
-    
+
     attr_reader :to_s
 
     def initialize(s)
-      
+
       super()
       @a = []
-    
+
       buffer, _ = RXFHelper.read(s)
 
       @doc = Rexle.new(buffer)
 
       @doc.root.xpath('records/test').map do |test|
-        
-        path, type, description = test.xpath('summary/*/text()')        
-        records = test.element('records')        
-        
+
+        path, type, description = test.xpath('summary/*/text()')
+        records = test.element('records')
+
         inputs = records.xpath('input/summary/*').map\
                                             {|x| [x.name, x.texts.join.strip]}
 
         outputs = records.xpath('output/summary/*').map\
                                             {|x| [x.name, x.texts.join.strip]}
-        
+
         @a << {type: type, in: inputs, out: outputs}
-        
+
       end
-    
+
     end # end of initialize()
-    
+
     def to_s()
 =begin
 s = %Q(
 require_relative "#{testgem}"
 require "test/unit"
- 
+
 class #{testclass} < Test::Unit::TestCase
- 
+
   def #{types[i]}
     #{@lines.join("\n    ")}
   end
-  
+
 end
 )
 =end
       #read the .rsf file
       script = @doc.root.element('summary/script/text()')
       raise 'script XML entry not found' unless script
-      
+
       if script then
-        filename = script[/[\/]+\.rsf$/] 
+        filename = script[/[\/]+\.rsf$/]
         buffer, _ = RXFHelper.read(filename)
       end
       #@a.group_by {|x| x[:type]}
-      
-      
+
+
     end
   end
 end
--- testdata.gemspec
+++ testdata.gemspec
@@ -1,17 +1,17 @@
 Gem::Specification.new do |s|
   s.name = 'testdata'
-  s.version = '1.1.10'
+  s.version = '1.1.11'
   s.summary = 'A test framework which accepts test data in a Polyrex format.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/testdata.rb']
   s.add_runtime_dependency('app-routes', '~> 0.1', '>=0.1.19')
   s.add_runtime_dependency('testdata_text', '~> 0.2', '>=0.2.2')
   s.add_runtime_dependency('diffyc32', '~> 0.1', '>=0.1.1')
-  s.add_runtime_dependency('polyrex', '~> 1.3', '>=1.3.1')
+  s.add_runtime_dependency('polyrex', '~> 1.3', '>=1.3.4')
   s.signing_key = '../privatekeys/testdata.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/testdata'
   s.required_ruby_version = '>= 2.1.2'
 end
